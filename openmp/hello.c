8182aca5235bac407a7a714b7d6d28798ae0e7c6
Vanya Yaneva <wyaneva@gmail.com>
2017-01-30 12:25:02 +0000

    wrote a toy OpenMP program

---
+++ openmp/hello.c
@@ -0,0 +1,16 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+  int nthreads, tid;
+  nthreads = omp_get_num_threads();
+  printf("Number of threads = %d\n", nthreads);
+ 
+#pragma omp parallel private(nthreads, tid)
+  {
+    tid = omp_get_thread_num();
+    printf("Hello World from thread - %d\n", tid);
+  }
+}
---
+++ openmp/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export OMP_NUM_THREADS=8
+./run-in-parallel

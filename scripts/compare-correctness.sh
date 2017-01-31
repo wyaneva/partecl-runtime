1d891f6f0c6254f157c6736d46832e12523e8e2b
Vanya Yaneva <wyaneva@gmail.com>
2017-01-31 19:09:29 +0000

    moved an execution script

--- scripts/compare-correctness.sh
+++ scripts/compare-correctness.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ./../build/gpu-test 131072 -results Y -time N -runs 1 > "$1-gpu-131072.correctness"
-./openmp-run.sh 131072 N Y 1 8 > "$1-cpu-131072.correctness"
+./../build/openmp-run.sh 131072 N Y 1 8 > "$1-cpu-131072.correctness"
 
 diff "$1-gpu-131072.correctness" "$1-cpu-131072.correctness" > "$1-131072-diff.correctness"

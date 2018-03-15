e370a73700b2deb0f2ed45791d7a88b77b5b0e48
Vanya Yaneva <wyaneva@gmail.com>
2018-03-15 16:10:31 +0000

    Running fsms on cpu

--- run-on-cpu/run-on-cpu.c
+++ run-on-cpu/run-on-cpu.c
@@ -16,6 +16,7 @@
 
 #include "../kernel-gen/cpu-gen.h"
 #include "../kernel-gen/structs.h"
+#include "../kernel-gen/fsm.h"
 #include "../utils/options.h"
 #include "../utils/read-test-cases.h"
 #include "../utils/timing.h"
@@ -23,12 +24,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../source/fsm.c"
 
-int run_main(struct partecl_input input, struct partecl_result *result);
+//int run_main(struct partecl_input input, struct partecl_result *result);
+int run_main(struct partecl_input input, struct partecl_result *result,
+             struct transition *transitions, int num_transitions,
+             int input_length, int output_length);
 
-void run_on_cpu(struct partecl_input input, struct partecl_result *result) {
+int main_cpu(int argc, char **argv, struct transition *transitions,
+             int *num_transitions, int *input_length, int *output_length) {
+  //if (argc < 3) {
+  //  printf("Please provide an input filename and an input sequence.\n");
+  //  return 0;
+  //}
+
+  // inputs
+  char *filename = argv[0];
+  //char *input_ptr = argv[2];
+
+  // read the fsm
+  // number of states
+  *num_transitions = read_parameter(filename, NUM_TRANSITIONS);
+  if (*num_transitions == -1) {
+    printf("File %s does not specify a number of transitions. Exiting. \n",
+           filename);
+    return 0;
+  }
+
+  // input length
+  *input_length = read_parameter(filename, INPUT_LENGTH);
+  if (*input_length == -1) {
+    printf("File %s does not specify input length. Exiting. \n", filename);
+    return 0;
+  }
+
+  // output length
+  *output_length = read_parameter(filename, OUTPUT_LENGTH);
+  if (*output_length == -1) {
+    printf("File %s does not specify output length. Exiting. \n", filename);
+    return 0;
+  }
+
+  // transitions
+  size_t size_transitions = sizeof(struct transition) * (*num_transitions);
+  transitions =
+      (struct transition *)malloc(size_transitions);
+  read_fsm(filename, transitions);
+
+ // original code ends here
+
+  return 1;
+}
+
+void run_on_cpu(struct partecl_input input, struct partecl_result *result,
+                struct transition *transitions, int num_transitions,
+                int input_length, int output_length) {
+
   result->test_case_num = input.test_case_num;
-  run_main(input, result);
+  run_main(input, result, transitions, num_transitions, input_length, output_length);
 }
 
 int main(int argc, char **argv) {
@@ -61,6 +114,14 @@ int main(int argc, char **argv) {
   // read the test cases
   if (read_test_cases(inputs, num_test_cases) == FAIL)
     return 0;
+
+  // read the fsm
+  char *filename = "train4.kiss2";
+  int num_transitions;
+  int input_length;
+  int output_length;
+  struct transition *transitions;
+  main_cpu(2, &filename, transitions, &num_transitions, &input_length, &output_length);
 
   for (int i = 0; i < num_runs; i++) {
     struct timespec time1, time2;

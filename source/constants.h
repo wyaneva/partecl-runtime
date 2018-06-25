98831781257de9c377ff09f98d58bfe7c1996d2a
Vanya Yaneva <wyaneva@gmail.com>
2018-06-25 14:53:15 +0100

    Added forgotten file for constant memory opt

---
+++ source/constants.h
@@ -0,0 +1,51 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+/**********************************
+ * Used in kernel for optimisations
+ **********************************/
+
+/*
+ * FSM_OPTIMISE toggles optimisations
+ *  1. coalesced memory allocation
+ *  2. hash table storage
+ */
+#ifndef FSM_OPTIMISE
+#define FSM_OPTIMISE 0
+#endif
+
+#if FSM_OPTIMISE
+#define TEST_INPUTS_ATTR global
+#else
+#define TEST_INPUTS_ATTR
+#endif
+
+/*
+ * FSM_CONSTANT_MEMORY and FSM_LOCAL_MEMORY 
+ * put the FSM in the corresponding memory spaces
+ * when there is enough space
+ */
+#ifndef FSM_CONSTANT_MEMORY
+#define FSM_CONSTANT_MEMORY 0
+#endif 
+
+#ifndef FSM_LOCAL_MEMORY
+#define FSM_LOCAL_MEMORY 0
+#endif
+
+#if FSM_CONSTANT_MEMORY
+#define FSM_ATTR constant
+#define FSM_ATTR_KNL constant
+#else 
+
+#if FSM_LOCAL_MEMORY
+#define FSM_ATTR local
+#define FSM_ATTR_KNL global
+#else
+#define FSM_ATTR global
+#define FSM_ATTR_KNL global
+#endif
+
+#endif
+
+#endif

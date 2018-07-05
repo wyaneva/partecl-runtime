b10892e4aa4d5c4487a8d39544c2f307e7ef18c1
Vanya Yaneva <wyaneva@gmail.com>
2018-07-05 14:04:59 +0100

    Added -sort as option to the openmp script

--- build/openmp-run.sh
+++ build/openmp-run.sh
@@ -6,7 +6,8 @@ DO_RESULTS=$3
 NUM_RUNS=$4
 NUM_THREADS=$5
 FSM_FILENAME=$6
+DO_SORT_TC=$7
 
 export OMP_NUM_THREADS=$NUM_THREADS
-./../build/cpu-test $NUM_TESTS -time $DO_TIME -results $DO_RESULTS -runs $NUM_RUNS -filename $FSM_FILENAME
+./../build/cpu-test $NUM_TESTS -time $DO_TIME -results $DO_RESULTS -runs $NUM_RUNS -filename $FSM_FILENAME -sort $DO_SORT
 

d8899677adb4f0b44ba0d4923552a48ac7b8ed23
Vanya Yaneva <wyaneva@gmail.com>
2018-07-05 15:36:46 +0100

    Really fixed the bug in openmp this time

--- build/openmp-run.sh
+++ build/openmp-run.sh
@@ -9,5 +9,5 @@ FSM_FILENAME=$6
 DO_SORT_TC=$7
 
 export OMP_NUM_THREADS=$NUM_THREADS
-./../build/cpu-test $NUM_TESTS -time $DO_TIME -results $DO_RESULTS -runs $NUM_RUNS -filename $FSM_FILENAME -sort $DO_SORT
+./../build/cpu-test $NUM_TESTS -time $DO_TIME -results $DO_RESULTS -runs $NUM_RUNS -filename $FSM_FILENAME -sort $DO_SORT_TC
 

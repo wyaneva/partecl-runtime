1cc2a4ef256fa186a41c2d8956bf097bf56dc6ab
Vanya Yaneva <wyaneva@gmail.com>
2018-08-08 05:56:04 +0100

    Correctness comparison script is more robust

--- scripts/compare-correctness.sh
+++ scripts/compare-correctness.sh
@@ -2,14 +2,16 @@
 
 NUM_TESTS=$1
 FSM_FILENAME=$2
-CHUNKSIZE=$3
+NUM_CPU_THREADS=$3
+DO_SORT=$4
+CHUNKSIZE=$5
 
 cd ../build
 make clean && make
 cd ../scripts
 
-./../build/gpu-test $NUM_TESTS -results Y -time N -runs 1 -filename $FSM_FILENAME -chunksize $CHUNKSIZE -sort Y > "gpu.correctness"
-./../build/openmp-run.sh $NUM_TESTS N Y 1 8 $FSM_FILENAME Y > "cpu.correctness"
+./../build/gpu-test $NUM_TESTS -results Y -time N -runs 1 -filename $FSM_FILENAME -chunksize $CHUNKSIZE -sort $DO_SORT > "gpu.correctness"
+./../build/openmp-run.sh $NUM_TESTS N Y 1 $NUM_CPU_THREADS $FSM_FILENAME Y > "cpu.correctness"
 
 diff "gpu.correctness" "cpu.correctness" 
 
